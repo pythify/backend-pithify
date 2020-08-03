0a251b2b23204c5de5cfb34f2357cd74ff5e719e
Jair Aguilar <apjair97@gmail.com>
2020-08-03 18:57:48 -0500

    Added search filter and next page json data was added

--- ceol/track_provider/spotiapi.py
+++ ceol/track_provider/spotiapi.py
@@ -11,8 +11,57 @@ class My_dictionary(dict):
     # Function to add key:value 
     def add(self, key, value): 
         self[key] = value 
+def songSearch(searchdict):
+    search_results = My_dictionary()
+    i = 0
+    for result in searchdict["data"]:
+                
+        my_dict = {}
+        my_dict['id'] = result["id"]
+        my_dict['title'] = result["title"]
+        my_dict['duration'] = result["duration"]
+        my_dict['url'] = result["preview"]
+        my_dict['artist_id'] = result["artist"]["id"]
+        my_dict['artist_name'] = result["artist"]["name"]
+        my_dict['album_id'] = result["album"]["id"]
+        my_dict['album_title'] = result["album"]["title"]
+        my_dict['album_tracklist'] = result["album"]["tracklist"]
+        my_dict['cover'] = result["album"]["cover"]
+        
+                
+        search_results.add( f'result{i}', json.dumps(my_dict))
+                
+        i += 1 
+    if searchdict['next']:
+        search_results['next'] = searchdict['next']
 
-def someSearch(query):
+
+    return search_results
+    
+def albumSearch(searchdict):
+    search_results = My_dictionary()
+    i = 0
+    my_list = []
+    for result in searchdict['data']:
+                
+        if not result["album"]["title"] in my_list:
+            my_list.append(result["album"]["title"])
+            my_dict = {}
+            my_dict['artist_name'] = result["artist"]["name"]
+            my_dict['album_title'] = result["album"]["title"]
+
+            tracklist_response = requests.request("GET", result["album"]["tracklist"])
+            json_tracklist = tracklist_response.text
+            my_dict['tracklist'] = json.loads(json_tracklist)['data']
+
+            my_dict['tracklist_url'] = result["album"]["tracklist"]
+            my_dict['cover'] = result["album"]["cover"]
+            search_results.add( f'result{i}', json.dumps(my_dict))
+            i += 1 
+
+    return search_results
+
+def someSearch(query,param):
 
 
     url = "https://deezerdevs-deezer.p.rapidapi.com/search"
@@ -26,28 +75,28 @@ def someSearch(query):
 
     response = requests.request("GET", url, headers=headers, params=querystring)
     json_data = response.text
-    searchdict = json.loads(json_data)["data"]
-    search_results = My_dictionary()
-    i = 0
-    for result in searchdict:
+    searchdict = json.loads(json_data)
+    if not searchdict:
+        param= 'error'
+    else:
+        # pprint.pprint(searchdict, sort_dicts=False)
+        if param == 'songs':
+            return songSearch(searchdict)
+        if param == 'artist':
+            return artistSearch(searchdict)
+        if param == 'genre':
+            return songSearch(searchdict)
+        if param == 'album':
+            return albumSearch(searchdict)
         
-        my_dict = {}
-        my_dict['id'] = result["id"]
-        my_dict['title'] = result["title"]
-        my_dict['duration'] = result["duration"]
-        my_dict['url'] = result["preview"]
-        my_dict['artist_id'] = result["artist"]["id"]
-        my_dict['artist_name'] = result["artist"]["name"]
-        my_dict['album_id'] = result["album"]["id"]
-        my_dict['album_title'] = result["album"]["title"]
-        my_dict['cover'] = result["album"]["cover"]
-        
-        search_results.add( f'result{i}', json.dumps(my_dict))
-        
-        i += 1        
-
-    return search_results
+    
+    
 
 if __name__ == "__main__":
-    r = someSearch('set fire to the rain')
-    pprint.pprint(r)
+    r = someSearch('eminen', 'songs')
+    next_response = requests.request("GET", r['next'])
+    json_next = next_response.text
+    searchdict1 = json.loads(json_next)
+    b = songSearch(searchdict1)
+    #print(r)
+    pprint.pprint(b, sort_dicts=False)
